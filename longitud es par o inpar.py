306499eeced54427ee7cb62544c392b4c2f4aebc
xyz <xyz@MacBook-Pro-Mobile-David.local>
2025-01-28 15:37:41 -0600

    Se realiza para verificar si una palabra su longitud es par o impar

---
+++ longitud es par o inpar.py
@@ -0,0 +1,19 @@
+
+print("ingrese una palabra")
+palabra=input()
+
+index=len(palabra)
+if index %2 == 0:
+   print("si es una palabra par")
+else:
+    print("es una palabra impar")
+
+
+
+
+
+
+
+
+
+
