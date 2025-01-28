7dd9d8a7dfdad261c7453e49239777c75767acf7
xyz <xyz@MacBook-Pro-Mobile-David.local>
2025-01-28 16:00:58 -0600

    Se renombraron los archivos

---
+++ longitud_es_par_o_inpar.py
@@ -0,0 +1,12 @@
+
+print("ingrese una palabra")
+palabra=input()
+
+index=len(palabra)
+
+if index %2 == 0:
+   print("Si es una palabra par")
+
+else:
+    print("Es una palabra impar")
+    
---
+++ numero_es_par_o_impar.py
@@ -0,0 +1,7 @@
+number=0
+
+while number % 2 == 0:
+    print("Ingrese un numero")
+    number=int(input())
+
+print("tu numero es impar")
