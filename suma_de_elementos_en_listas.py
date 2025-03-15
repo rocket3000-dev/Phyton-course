6bade4d672eba26d5a004102c72089312ee3832c
xyz <xyz@MacBook-Pro-Mobile-David.local>
2025-03-15 09:54:44 -0600

    suma los numeros de una lista

---
+++ suma_de_elementos_en_listas.py
@@ -0,0 +1,27 @@
+number=int(input())
+list_number=[]
+index=0
+result=0
+while number != -1:
+    print("ingrese un numero o escriba -1 para finalizar")
+    number=int(input())
+
+    if number != -1:
+        list_number.append(number)
+
+for m in list_number:
+    result+=list_number[index]
+
+    index+=1
+
+print(list_number)
+print(result)
+
+
+
+
+
+
+
+
+
