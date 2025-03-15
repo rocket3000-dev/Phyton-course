f8f5a08d8b9cbb2d19a9ce2f89dd1f9a02c10e20
xyz <xyz@MacBook-Pro-Mobile-David.local>
2025-03-15 11:01:18 -0600

    Este codigo sirve para invertir una lista

---
+++ invertir_listas.py
@@ -0,0 +1,17 @@
+
+number=""
+list_number=[]
+
+while number != "-1":
+    print("ingrese un numero o escriba -1 para acabar")
+    number=input()
+
+    if number != "-1":
+        list_number.append(number)
+
+index_externo=len(list_number)
+
+for elemento in list_number: 
+    print(f"indice: {index_externo}, valor:{list_number[index_externo]}")
+
+    index_externo-=1
