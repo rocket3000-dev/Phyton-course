e415ad45d2ff85e12eed5d2696cf3b2fe0a1e7f8
xyz <xyz@MacBook-Pro-Mobile-David.local>
2025-04-07 14:51:13 -0600

    Este codigo es como el diagonal_1_matrices pero imprime del lado derecho superior

---
+++ diagonal_2_matrices.py
@@ -0,0 +1,14 @@
+matris=[
+    [0, 1, 2, 3, 4]  ,  #fila 0
+    [5, 6, 7, 8, 9]  ,  #fila 1
+    [10,11,12,13,14] ,  #fila 2
+    [15,16,17,18,19] ,  #fila 3
+    [20,21,22,23,24]    #fila 4
+]    
+
+fila=5
+columna=4
+
+for diagonal in range(fila):
+    print(f"fila {columna} : {matris[diagonal] [columna]}")
+    columna-=1
